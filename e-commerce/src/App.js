ecf8047b36723e0e670a93fd78172ab6c35370dc
Byakuya91 <nganti91@yahoo.com>
2023-03-05 17:45:24 -0500

    I've tired to load in the routes to see if it will work. I will try to implement a NavBar at some point but I still need to test each of the links in the routeto see if it works. All in all, it is unlikely I'll be able to complete this during the three day sprint and will need to be extended to Monday.

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import Header from "./containers/Header";
 import ProductComponent from "./containers/ProductComponent";
 import ProductDetail from "./containers/ProductDetail";
+import ProductListing from "./containers/ProductListing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
+  // TODOS;
+  // 1) Improve the routing
+  // 2) Look up documentation about Routing specifically with paths
+
   return (
     <div className="App">
       <Header />
-      {/* <ProductComponent />
-      <ProductDetail />
-      <h1>Hello World!</h1> */}
+      <Routes>
+        <Route path="/" element={ProductListing} />
+        <Route path="/product/:productId" element={ProductDetail} />
+        <Route>404 not found</Route>
+      </Routes>
     </div>
   );
 }
--- e-commerce/src/containers/ProductListing.js
+++ e-commerce/src/containers/ProductListing.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ProductListing = () => {
+  // Fetch the product listed in the store.Js
+  const products = useSelector((state) => state);
+  console.log(products);
+
   return (
-    <div>
+    <div className="ui grid container">
       {" "}
       <h1> ProductListing</h1>
     </div>
