14abacab76dae73e72ec627efd479fe309e9ad04
Byakuya91 <nganti91@yahoo.com>
2023-03-05 15:17:41 -0500

    The Product Component seems to be displaying on App.JS but for some reason, the header is not working. I'll be sure to look into this later after I get the routing setup.

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./containers/Header";
-
+import ProductComponent from "./containers/ProductComponent";
 function App() {
   return (
     <div className="App">
+      <Header />
+      <ProductComponent />
       <h1>Hello World!</h1>
     </div>
   );
