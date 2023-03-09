009a5b083fd5db6db30658d1c93247a771e8430d
Byakuya91 <nganti91@yahoo.com>
2023-03-09 13:45:56 -0500

    I've removed the four column grid which resolved the bunched up CSS as cards. Padding and spacing stuff can come later but as it stands, the shop looks a lot better. Moving onto the next step which is linking the cards to separate pages.

--- e-commerce/src/containers/ProductComponent.js
+++ e-commerce/src/containers/ProductComponent.js
@@ -11,20 +11,20 @@ const ProductComponent = () => {
     // destructuring the product object
     const { id, title, image, price, category } = product;
     return (
-      <div className="four column wide" key={id}>
-        <div className="ui link cards">
-          <div className="card">
-            <div className="image">
-              <img src={image} alt={title} />
-            </div>
-            <div className="content">
-              <div className="header">{title}</div>
-              <div className="meta price">$ {price}</div>
-              <div className="meta ">{category}</div>
-            </div>
+      // <div className="four column equal grid" key={id}>
+      <div className="ui link cards" key={id}>
+        <div className="card">
+          <div className="image">
+            <img src={image} alt={title} />
+          </div>
+          <div className="content">
+            <div className="header">{title}</div>
+            <div className="meta price">$ {price}</div>
+            <div className="meta ">{category}</div>
           </div>
         </div>
       </div>
+      // </div>
     );
   });
 
