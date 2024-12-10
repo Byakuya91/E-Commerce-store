27a92597f29598cbff205dff64e244c467370d40
Byakuya91 <nganti91@yahoo.com>
2024-12-10 18:27:57 -0500

    I've added a shopping cart page and a Shopping cart component. I will be integrating them tomorrow and figuring out a way to display the items within the cart.

---
+++ e-commerce/Components/ShoppingCart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoppingCart = () => {
+  return <div>ShoppingCartPage</div>;
+};
+
+export default ShoppingCart;
---
+++ e-commerce/Pages/shoppingCartPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const shoppingCartPage = () => {
+  return <div>shoppingCartPage</div>;
+};
+
+export default shoppingCartPage;
