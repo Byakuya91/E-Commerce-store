81afb7f3ab33376a4cf9772123ccc61fa8365838
Byakuya91 <nganti91@yahoo.com>
2024-12-30 06:21:19 -0500

    I've done troubleshooting on the delayed response for the shopping cart page. I'm working on implementing a loading spinner using react loader spinner as a temporary solution before I try to figure out ways to get the API data a lot faster. Once I put in the loading spinner I want to focus on aggregating products. So if I order the same product that's already in the cart, it will add to the quantity as opposed to being a new item.

--- e-commerce/src/containers/ProductListing.js
+++ e-commerce/src/containers/ProductListing.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setProducts } from "../redux/actions/productActions";
 import axios from "axios";
@@ -7,10 +7,12 @@ import ProductComponent from "./ProductComponent";
 const ProductListing = () => {
   // Fetch the product listed in the store.Js
   const products = useSelector((state) => state);
-  // console.log(products);
-
   // Dispatch the action through useDispatch: A redux hook
   const dispatch = useDispatch();
+  // loading state
+  const [loading, setLoading] = useState(true); // Add loading state
+
+  // console.log("the loading state is: ", loading);
 
   // TODOs:
   //  1) Create API function and UseEffect(DONE)
@@ -19,22 +21,31 @@ const ProductListing = () => {
 
   // 1) API function
   const retrieveProducts = async () => {
-    // define response variable
-    const response = await axios
-      .get("https://fakestoreapi.com/products")
-      // Tracking for any errors during the call
-      .catch((err) => {
-        console.log("Err", err);
-      });
-    // Dispatching an action
-    dispatch(setProducts(response.data));
+    // console.log("Before API call, loading state:", loading);
+    console.time("API fetch time");
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      dispatch(setProducts(response.data));
+      setLoading(false); // Set loading to false once data is fetched
+    } catch (err) {
+      console.error("Error fetching products", err);
+      setLoading(false); // Ensure loading state is updated even on error
+    }
+    // console.log("After API call, loading state:", loading);
+    console.timeEnd("API fetch time");
   };
 
   // 2) Call the retrieveProducts through a UseEffect
   useEffect(() => {
     retrieveProducts();
   }, []);
-  // console.log("Products:", products);
+
+  // Checking API and state flow
+  // console.log("Products from Redux store:", products);
+
+  if (loading) {
+    return <div>Loading product details...</div>;
+  }
 
   return (
     <div className="ui grid container">
