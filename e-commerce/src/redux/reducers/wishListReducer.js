1db45f86b162ae247c732690035941e771e4e987
Byakuya91 <nganti91@yahoo.com>
2025-04-23 11:39:56 -0400

    Set up intial wishList reducer actions and began coding. I will need to look into find array method and see how it works. The goal will be to code out at minimum the add to Wish-list reducer.

--- e-commerce/src/redux/reducers/wishListReducer.js
+++ e-commerce/src/redux/reducers/wishListReducer.js
@@ -1,7 +1,26 @@
 // TODO: Code out the wishlist reducer
 
+import { ActionTypes } from "../constants/action-types";
+
 const wishListState = {
   products: [], // Array to hold products in the cart
 };
 
-// export const wishListReducer = (state = wishListState, action) => {};
+export const wishListReducer = (state = wishListState, action) => {
+  switch (action.type) {
+    case ActionTypes.ADD_TO_WISHLIST:
+      return {
+        // ? Check if a product exists(ONGOING)
+        ...state,
+        products: [...state.products, action.payload],
+
+        // ? if it doesn't exist, add it to the wishList
+        // ? Return the state unchanged if the product is already in the wishList
+      };
+
+      break;
+
+    default:
+      break;
+  }
+};
